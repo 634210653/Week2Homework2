8dc9e31d4c6737bbeba9b87391084978c9a14368
LP_PCPC_Master <634210653@qq.com>
2019-04-04 12:59:41 +0800

    pass myMapTest

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MyMap {
@@ -17,22 +19,52 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+
+        List<Integer> res = new ArrayList<Integer>();
+        for(int num : array){
+           res.add(num*3);
+        }
+        return res;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+
+        List<String> res = new ArrayList<String>();
+        for(int num : array){
+            res.add(letters[num-1]);
+           // res.add(new Character((char)(num+96)).toString());
+        }
+        return res;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> res = new ArrayList<String>();
+        for(int num : array){
+            StringBuffer str = new StringBuffer();
+            for(int cur = num; cur != 0; ){
+                int key = (cur-1)%26;
+                str.insert(0, letters[key]);
+                cur = (cur-1)/26;
+            }
+            res.add(str.toString());
+        }
+        return res;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        res.addAll(array);
+        res.sort(Comparator.reverseOrder());
+        return res;
+
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        res.addAll(array);
+        res.sort(Comparator.naturalOrder());
+        return res;
     }
 }
